cf58c8c720e81f8d53b8d8223fcb5c48ab63f6e3
Robert <90513724+ideen-los@users.noreply.github.com>
2023-08-30 00:41:49 +0200

    ENH: add dialog and corresponding function to add new books to library

--- js/library.js
+++ js/library.js
@@ -1,3 +1,4 @@
+/* DEFINE LIBRARY ARRAY & BOOK CONSTRUCTOR */
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -7,16 +8,13 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+/* DEFINE FUNCTION TO ADD A NEW BOOK TO THE LIBRARY */
 function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
 
+/* DEFINE BOOK CONTRAINER & FUNCTION TO DISPLAY THE LIBRARY */
 const container = document.querySelector("#container");
-
-addBookToLibrary("The Bible", "Johannes", 1988, false);
-addBookToLibrary("Robert's memoirs", "Robert Arning", 1, true);
-addBookToLibrary("The Swan", "Dr. Frank Pechart", 321, true);
-addBookToLibrary("I was once lost", "Machiavelli", 864, false);
 
 const displayLibrary = function () {
   for (book of myLibrary) {
@@ -47,4 +45,37 @@ const displayLibrary = function () {
   }
 };
 
-displayLibrary();
+/* ADD EVENTLISTENERS TO TRIGGER ADD-A-NEW-BOOK DIALOG & SUBMIT THE FORM */
+const addBookDialog = document.querySelector("#add-book-dialog");
+
+const showDialogButton = document.querySelector(".show-btn");
+showDialogButton.addEventListener("click", (e) => {
+  addBookDialog.showModal();
+});
+
+const addBookForm = document.querySelector("#add-book-form");
+addBookForm.addEventListener("submit", submitForm);
+
+/* GET VALUE OF FORM INPUTS AND CALL ADD-A-NEW-BOOK FUNCTION ON SUBMIT */
+function submitForm(e) {
+  let addBookTitle = document.querySelector("#title");
+  let addBookAuthor = document.querySelector("#author");
+  let addBookPages = document.querySelector("#pages");
+  let addBookRead = document.querySelector("#read");
+
+  addBookToLibrary(
+    addBookTitle.value,
+    addBookAuthor.value,
+    addBookPages.value,
+    addBookRead.value
+  );
+
+  container.innerHTML = "";
+  displayLibrary();
+  e.preventDefault();
+  addBookTitle.value = "";
+  addBookAuthor.value = "";
+  addBookPages.value = "";
+  addBookRead.value = "";
+  addBookDialog.close();
+}
